63189197f08a5507672673171b3c5554abea53b0
Fritz Pilz <pilzf98@zedat.fu-berlin.de>
2023-02-16 23:56:36 +0100

    replace while loops in container with for loops

--- examples/ks_test/bpf/kolmogorov_smirnov_test.c
+++ examples/ks_test/bpf/kolmogorov_smirnov_test.c
@@ -1,17 +1,24 @@
 #include "kolmogorov_smirnov_test.h"
 
 KS_Test_Results* kolmogorov_smirnov_test(KS_Context* ctx){
-	int F_s1[STEPS]; int F_s2[STEPS]; int observations1 = 0.0; int observations2 = 0.0; int max = 0.0;
+	int F_s1[STEPS]; int F_s2[STEPS]; int observations1 = 0; int observations2 = 0; int max = 0;
     //get the occurences within i*STEP > x >= (i+1)*STEP from the first array and save them in function F_s1
 	for(int i = 1; i <= STEPS; ++i){
-		while(i*STEP <= ctx->setup->arr1[(int)observations1] && observations1 < ctx->setup->len1){
+		// TODO: Fix condition in for-loop to take previous iterations of the outer loop into account (i.e. the new length)
+		for(int j = 0; j < ctx->setup->len1; ++j){
+			if(i*STEP <= ctx->setup->arr1[observations1]){
+				break;
+			}
 			++observations1;
 		}
 		F_s1[i-1] = observations1;
 	}
     //get the occurences within i*STEP > x >= (i+1)*STEP from the second array and save them in function F_s2
 	for(int i = 1; i <= STEPS; ++i){
-		while(i*STEP <= ctx->setup->arr2[(int)observations2] && observations2 < ctx->setup->len2){
+		for(int j = 0; j < ctx->setup->len2; ++j){
+			if(i*STEP <= ctx->setup->arr2[observations2]){
+				break;
+			}
 			++observations2;
 		}
 		F_s2[i-1] = observations2;
